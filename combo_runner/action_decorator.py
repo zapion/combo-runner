49c8d70a85aaf0fedf721f4a1c87e3f9b20ddcc1
Paul Zapion Yang <zapionator@gmail.com>
2015-01-05 17:26:45 +0800

    Accepting function argument.  Action directly controls function logic

--- combo_runner/action_decorator.py
+++ combo_runner/action_decorator.py
@@ -1,13 +1,12 @@
 def action(func):
-    def func_wrapper(self):
+    def func_wrapper(self, **kwargs):
         if func.__name__ in self.settings:
             if self.settings[func.__name__]:
-                self.logger.debug('The setting [%s] is true.' % func.__name__)
-                return func(self, action=True)
+                self.logger.debug('Task [%s] is enabled.' % func.__name__)
+                return func(self, action=True, **kwargs)
             else:
-                self.logger.debug('The setting [%s] is false.' % func.__name__)
-                return func(self, action=False)
+                self.logger.debug('Task [%s] is disabled.' % func.__name__)
         else:
             self.logger.debug('There is no [%s] in settings file, it will run with default settings.' % func.__name__)
-            return func(self, action=True)
+            return func(self, **kwargs)
     return func_wrapper
