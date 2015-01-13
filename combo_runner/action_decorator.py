48a85274833737340fb5cc45f53be4783d453e06
Paul Zapion Yang <zapionator@gmail.com>
2015-01-13 18:04:25 +0800

    modify parameter name action -> enabled

--- combo_runner/action_decorator.py
+++ combo_runner/action_decorator.py
@@ -1,15 +1,16 @@
-def action(func):
-    def func_wrapper(self, **kwargs):
-        if func.__name__ in self.settings:
-            if self.settings[func.__name__]:
-                self.logger.debug('Task [%s] is enabled.' % func.__name__)
-                return func(self, enabled=True, **kwargs)
+def action(enabled=True):
+    def f_action(func):
+        def func_wrapper(self, **kwargs):
+            if func.__name__ in self.settings:
+                if self.settings[func.__name__]:
+                    self.logger.debug('Task [%s] is enabled.' % func.__name__)
+                    return func(self, enabled=True, **kwargs)
+                else:
+                    self.logger.debug('Task [%s] is disabled.' % func.__name__)
             else:
+                self.logger.debug('There is no [%s] in settings file, it will run with default settings.' % func.__name__)
+                if enabled:
+                    return func(self, **kwargs)
                 self.logger.debug('Task [%s] is disabled.' % func.__name__)
-        else:
-            self.logger.debug('There is no [%s] in settings file, it will run with default settings.' % func.__name__)
-            if enabled:
-                return func(self, **kwargs)
-            else:
-                self.logger.debug('Task [%s] is disabled.' % func.__name__)
-    return func_wrapper
+        return func_wrapper
+    return f_action
--- examples/test/test_action_runner.py
+++ examples/test/test_action_runner.py
@@ -6,26 +6,23 @@ class TestActionRunner(BaseActionRunner):
 
     action = action_decorator.action
 
-    @action
-    def do_test_pre(self, action=False):
-        if action:
-            self.pre_commands.append('rm -rf .env; virtualenv .env; source .env/bin/activate; pip install mozdownload')
-            self.pre_commands.append('mozdownload -h')
-            self.pre_commands.append('export TEAM=MOZTWQA')
-            self.pre_commands.append('./test_pre.sh')
+    @action(enabled=False)
+    def do_test_pre(self):
+        self.pre_commands.append('rm -rf .env; virtualenv .env; source .env/bin/activate; pip install mozdownload')
+        self.pre_commands.append('mozdownload -h')
+        self.pre_commands.append('export TEAM=MOZTWQA')
+        self.pre_commands.append('./test_pre.sh')
         return self
 
     # TODO: for testing
-    @action
-    def do_test(self, action=False):
-        if action:
-            self.commands.append('./test.sh')
+    @action(enabled=False)
+    def do_test(self):
+        self.commands.append('./test.sh')
         return self
 
     # TODO: for testing
-    @action
-    def do_test_post(self, action=False):
-        if action:
-            self.post_commands.append('mozdownload -h')
-            self.post_commands.append('./test_post.sh')
+    @action(enabled=False)
+    def do_test_post(self):
+        self.post_commands.append('mozdownload -h')
+        self.post_commands.append('./test_post.sh')
         return self
